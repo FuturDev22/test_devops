865eb0dd03813fffc642566959b1e4fd05d5599e
Souissi oumaima <99618076+FuturDev22@users.noreply.github.com>
2024-07-11 21:28:40 +0100

    Update test_login_bdd.py

--- tests/test_login_bdd.py
+++ tests/test_login_bdd.py
@@ -24,10 +24,10 @@ def user_open_login_page(driver):
 def enter_credentials(driver):
     username_locator = WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.ID, "username")))
     username_locator.send_keys("student")
-    time.sleep(2)
+    time.sleep(1)
     password_locator = WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.NAME, "password")))
     password_locator.send_keys("Password123")
-    time.sleep(2)
+    time.sleep(1)
 
 @when("L\'utilisateur clique sur submit")
 @allure.step("L'utilisateur clique sur submit")
@@ -40,18 +40,20 @@ def submit_click(driver):
 def voir_url(driver):
     actual_url = driver.current_url
     assert actual_url == "https://practicetestautomation.com/logged-in-successfully/"
-    
+    time.sleep(1)
+
 @then("L\'utilisateur doit voir le message Logged In Successfully")
 @allure.step("L'utilisateur doit voir le message Logged In Successfully")
 def voir_message(driver):
     text_locator = driver.find_element(By.TAG_NAME, "h1")
     actual_text = text_locator.text
     assert actual_text == "Logged In Successfully"
-    
+    time.sleep(1)
+
 @then("L\'utilisateur doit voir le bouton de déconnexion")
 @allure.step("L'utilisateur doit voir le bouton de déconnexion")
 def voir_bouton(driver):
     log_out_button_locator = driver.find_element(By.LINK_TEXT, "Log out")
     assert log_out_button_locator.is_displayed()
-    time.sleep(5)
+    time.sleep(2)
 
