ceb80eb790f3db55ff2084d0e99d99309af438a7
Souissi oumaima <99618076+FuturDev22@users.noreply.github.com>
2024-07-11 18:17:13 +0100

    Update test_login_bdd.py

--- tests/test_login_bdd.py
+++ tests/test_login_bdd.py
@@ -41,7 +41,7 @@ def connexion_click(driver):
 @allure.step("l'utilisateur devrait voir un message d'erreur")
 def error_message(driver):
        WebDriverWait(driver, 60).until(
-        EC.visibility_of_element_located((By.XPATH, "//html[@id='facebook']//div[@id='loginform']//div[@class='_9ay7'][contains(text(), 'The password you’ve entered is incorrect.')]"))
+            EC.visibility_of_element_located((By.XPATH, "//html[@id='facebook']/body/div[3]/div[@role='dialog']//div[@class='_9kq2'][contains(text(), 'Nous n’avons pas trouvé de compte correspondant à ce que vous avez entré, mais nous en avons trouvé un dont la correspondance est proche.')]"))
     )
 
 
